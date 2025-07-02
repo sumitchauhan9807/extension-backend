4ddf50725d62eeca8863b7b43ca44aa4f3a780b4
Sumit chauhan <sumitchauhan9807666@gmail.com>
2025-07-02 12:51:39 +0530

    wip

--- src/controllers/admin/controller.ts
+++ src/controllers/admin/controller.ts
@@ -172,6 +172,7 @@ class AdminController {
     try {
       let response = new ResponseHandler();
       let allUsers = await User.find({
+        relations:['chats'],
         order: {
           createdAt: "DESC",
         },
